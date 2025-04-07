e1790be2528527fe50aac2f2dd64498ceaed3bf6
CodeBase-Amit <sharma.amit262004@gmail.com>
2025-04-08 00:02:57 +0530

    Update avatar images and rename AI Interviewer to Prep Bot in Agent component

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -87,15 +87,15 @@ const Agent = ({ userName, userId, type }: AgentProps) => {
         <div className="call-view">
             <div className="card-interviewer">
                 <div className="avatar">
-                    <Image src="/ai-avatar.png" alt="vapi" width={65} height={54} className="object-cover" />
+                    <Image src="/robo_logo.png" alt="vapi" width={65} height={54} className="object-cover" />
                     {isSpeaking && <span className="animate-speak" />}
                 </div>
-                <h3>AI Interviewer</h3>
+                <h3>Prep Bot</h3>
             </div>
 
             <div className="card-border">
                 <div className="card-content">
-                    <Image src="/user-avatar.png" alt="user avatar" width={540} height={540} className="rounded-full object-cover size-[120px]" />
+                    <Image src="/man-avatar-icon-free-vector.png" alt="user avatar" width={540} height={540} className="rounded-full object-cover size-[120px]" />
                     <h3>{userName}</h3>
                 </div>
             </div>
